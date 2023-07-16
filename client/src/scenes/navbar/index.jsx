67e0ba9f1ace06203c199b2ebd4080bb50fb4629
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-07-17 05:13:46 +0530

    Color Theme, Dark Mode and Styling Setup added

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,18 @@
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useMediaQuery, useTheme} from "@mui/material";
+
 const Navbar = () => {
+
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+
     return (
         <div>
             <h1>Navbar</h1>
